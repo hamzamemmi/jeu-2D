8f842b46c6091cad319bad7fbbe6842b056473a3
oueslatikarim <47775685+oueslatikarim@users.noreply.github.com>
2019-03-13 15:50:24 +0100

    affi, init  ennemi  et deplacement a gauche et a droite ennemi

--- main.c
+++ main.c
@@ -1,116 +1,72 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <SDL/SDL.h>
-#include <SDL/SDL_mixer.h>
-#include <SDL/SDL_image.h>
-#include <string.h>
-#include <time.h>
-#include "enig.h"
 
-//void afficher_enigme(SDL_Surface ecran,char image[20]);
 
 int main(int argc, char *argv[])
 {
-srand(time(NULL));
-int nbalea=0;
-int *x1=0,*y1=0,*x2=0,*y2=0,*x3=0,*y3=0;
-char image[20],categorie1[20]="h",categorie2[20]="g",categorie3[20]="p";
 
-SDL_Surface *ecran = NULL, *imagefond=NULL, *enigme=NULL;
-
-SDL_Rect positionFond;
+    SDL_Surface *fond = NULL, *pelouse = NULL, *vache = NULL;
+    SDL_Rect positionPelouse, positionVache;
+    int continuer = 1, droite = 1;
     SDL_Event event;
-    int continuer = 1;
+ 
 
-    positionFond.x = 0;
-    positionFond.y = 0;
+    SDL_Init(SDL_INIT_VIDEO);
+  
 
-SDL_Init(SDL_INIT_VIDEO); // Initialisation de la SDL
+    fond = SDL_SetVideoMode(1280,640,32,SDL_HWSURFACE);
+    pelouse = SDL_LoadBMP("background.bmp");
+    vache = SDL_LoadBMP("enemy1battleportrait.bmp");
 
-ecran = SDL_SetVideoMode(1024, 415, 32, SDL_HWSURFACE); // Ouverture de la fenêtre
+    SDL_SetColorKey(vache, SDL_SRCCOLORKEY,SDL_MapRGB(vache->format,0,0,255));
+ 
 
-SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 255, 255, 255));
+    SDL_WM_SetCaption("Mouvement",NULL);
+ 
 
-SDL_WM_SetCaption("IN GAME", NULL);
+    SDL_FillRect(fond,NULL,SDL_MapRGB(fond->format,255,0,0));
+ 
 
-imagefond = IMG_Load("fond.png");
+    positionPelouse.x = 0;
+    positionPelouse.y = 0;
+    positionVache.x = 700;
+    positionVache.y = 400;
+ 
 
-SDL_BlitSurface(imagefond, NULL, ecran, &positionFond);
-while (continuer)
+    SDL_BlitSurface(pelouse,NULL,fond,&positionPelouse);
+    SDL_BlitSurface(vache,NULL,fond,&positionVache);
+ 
 
-{
+    while(continuer) {
+        SDL_PollEvent(&event);
+        switch (event.type) {
+            case SDL_QUIT :
+                continuer = 0;
+                 break;   
+        }
+   
+        if (positionVache.x > 900) droite = 0;
+        if (positionVache.x <= 600) droite = 1;
+   
 
-    SDL_PollEvent(&event);//attendre l'evenement
+        if (droite) positionVache.x++;
+        else positionVache.x--;
+   
+        SDL_Delay(10); 
+        SDL_FillRect(fond,NULL,SDL_MapRGB(fond->format,255,255,255));
+        SDL_BlitSurface(pelouse,NULL,fond,&positionPelouse);
+        SDL_BlitSurface(vache,NULL,fond,&positionVache); 
 
-    switch(event.type)
+        SDL_Flip(fond);
+    }
+ 
 
-    {
-        case SDL_KEYDOWN:
-            switch (event.key.keysym.sym)
-
-            {
-                case SDLK_ESCAPE: /* Appui sur la touche Echap, on arrête le programme */
-                    //continuer = 0;
-ecran = SDL_SetVideoMode(1024, 415, 32, SDL_HWSURFACE); // Ouverture de la fenêtre
-
-SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 255, 255, 255));
-                    SDL_BlitSurface(imagefond, NULL, ecran, &positionFond);
-SDL_Flip(ecran);
-                    break;
-            }//event.key.keysym.sym
-
-            break;
-            case SDL_QUIT: /* Si c'est un événement QUITTER */
-            continuer = 0; /* On met le booléen à 0, donc la boucle va s'arrêter */
-            break;
-case SDL_MOUSEBUTTONDOWN:
-switch(event.button.button)
-{
-case SDL_BUTTON_LEFT:
-if((event.button.x<316.3)&&(event.button.x>101)&&(event.button.y>44)&&(event.button.y<335.6))
-            {
-            strcpy(image,"");
-            strcpy(categorie1,"h");
-            nbalea=1+rand()%2;
-            sprintf(image,"%d",nbalea);
-            strcat(image,".png");
-            strcat(categorie1,image);
-            strcpy(image,categorie1);
-            afficher_enigme(image);
-            SDL_Flip(ecran);//mise a jour ecran
-            }
-else if((event.button.x<616.2)&&(event.button.x>401.8)&&(event.button.y>44)&&(event.button.y<335.6))
-            {
-            strcpy(image,"");
-            strcpy(categorie2,"g");
-            nbalea=1+rand()%3;
-            sprintf(image,"%d",nbalea);
-            strcat(image,".png");
-            strcat(categorie2,image);
-            strcpy(image,categorie2);
-            afficher_enigme(image);
-            SDL_Flip(ecran);//mise a jour ecran
-            }
-else if((event.button.x<920)&&(event.button.x>712.2)&&(event.button.y>44)&&(event.button.y<335.6))
-            {
-            strcpy(image,"");
-            strcpy(categorie3,"h");
-            sprintf(image,"%d",nbalea);
-            strcat(image,".png");
-            strcat(categorie1,image);
-            strcpy(image,categorie1);
-            afficher_enigme(image);
-            SDL_Flip(ecran);//mise a jour ecran
-            }
-break;
-}//event.button.button
-    }//switch(event.type)
-
-}//while
-SDL_Flip(ecran);//mise a jour ecran
-
-    SDL_FreeSurface(imagefond);
-    SDL_FreeSurface(enigme);
-    SDL_Quit(); // Arrêt de la SDL
-    return EXIT_SUCCESS; // Fermeture du programme
+    SDL_FreeSurface(pelouse); 
+    SDL_FreeSurface(vache);
+    SDL_Quit();
+ 
+    return EXIT_SUCCESS;
+ 
 }
+
