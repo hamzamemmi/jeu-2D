26ccabfc481d4eed46b4db028e5a5898f4f9caac
ziedrazouane <47760491+ziedrazouane@users.noreply.github.com>
2019-03-13 15:46:40 +0100

    Update enigme.c

--- enigme.c
+++ enigme.c
@@ -32,17 +32,3 @@ SDL_BlitSurface(enigme, NULL, fenetreenigme, &positionenigme);
 SDL_Flip(fenetreenigme);
 }
 
-void resolution_enigme(char image[20])
-{
-char solution[20]="s";
-strcat(solution,image);
-strcpy(image,solution);
-SDL_Surface *fenetreenigme=NULL,*enigme=NULL;
-SDL_Rect positionenigme;
-    positionenigme.x = 0;
-    positionenigme.y = 0;
-fenetreenigme = SDL_SetVideoMode(701, 698, 32, SDL_HWSURFACE);
-enigme = IMG_Load(image);
-SDL_BlitSurface(enigme, NULL, fenetreenigme, &positionenigme);
-SDL_Flip(fenetreenigme);
-}
